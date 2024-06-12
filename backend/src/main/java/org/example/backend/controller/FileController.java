9c2bda3e1b3665a186044c89ebc5f6f0be6038a4
Ramazon Yaxyoyev <ramazonyaxyoyev517@gmail.com>
2024-06-12 11:15:30 +0500

    asdasd

---
+++ backend/src/main/java/org/example/backend/controller/FileController.java
@@ -0,0 +1,57 @@
+package org.example.backend.controller;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    @PostMapping("/img")
+    public String saveProductImg(@RequestParam MultipartFile file) throws IOException {
+        String name= UUID.randomUUID()+file.getOriginalFilename();
+        FileOutputStream fileOutputStream = new FileOutputStream("backend/resors/"+name);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return name;
+    }
+    @GetMapping("/img")
+    public void getImg(HttpServletResponse response,@RequestParam String name) throws IOException {
+        FileInputStream fileInputStream=new FileInputStream(
+                "backend/resors/"+name);
+        ServletOutputStream outputStream = response.getOutputStream();
+        fileInputStream.transferTo(outputStream);
+        fileInputStream.close();
+        outputStream.close();
+    }
+    @PostMapping("/video")
+    public String saveProductVideo(@RequestParam MultipartFile file) throws IOException {
+        String name = UUID.randomUUID() + file.getOriginalFilename();
+        FileOutputStream fileOutputStream = new FileOutputStream("backend/resors/" + name);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return name;
+    }
+    @GetMapping("/video")
+    public void getVideo(HttpServletResponse response, @RequestParam String name) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("backend/resors/" + name);
+        ServletOutputStream outputStream = response.getOutputStream();
+        fileInputStream.transferTo(outputStream);
+        fileInputStream.close();
+        outputStream.close();
+    }
+
+
+}
