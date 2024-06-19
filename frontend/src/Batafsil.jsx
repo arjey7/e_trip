22d1e147a446c53e2946a332087ec3d28a59ce31
Ismoil <mahbubatilaeva@gmail.com>
2024-06-19 16:20:06 +0500

    Comment

--- frontend/src/Batafsil.jsx
+++ frontend/src/Batafsil.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef, useState} from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from "axios";
 import './css/Batafsil.css';
 import Comment from "./Comment.jsx";
@@ -13,9 +13,10 @@ import y from "./batafilimg/Vector.png";
 import u from "./batafilimg/rasm.png";
 import i from "./batafilimg/Vector (1).png";
 import o from "./batafilimg/Union.png";
-import p from "./batafilimg/rasmcha.png"
+import p from "./batafilimg/rasmcha.png";
 import Rodal from "rodal";
 import EnquiryForm from "./EnquiryForm.jsx";
+
 function Batafsil() {
     const { tourId } = useParams();
     const [batafsil, setBatafsil] = useState([]);
@@ -27,10 +28,12 @@ function Batafsil() {
     const [day, setDay] = useState(0);
     const initialTop = 680;
     const increment = 200;
-    const videoRef = useRef(null);
-    const navigate = useNavigate();
-    const [playingVideo, setPlayingVideo] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
+
+    const groupToursRef = useRef(null);
+    const aboutUsRef = useRef(null);
+    const contactRef = useRef(null);
+    const footerRef = useRef(null);
 
     useEffect(() => {
         axios.get(`http://localhost:8082/api/tourDay/${tourId}`)
@@ -55,58 +58,34 @@ function Batafsil() {
         setModalVisible(true);
     }
 
-
     const handleCloseModal = () => {
         setModalVisible(false);
     };
-    const handleVideoPlay = (videoSrc) => {
-        if (playingVideo && playingVideo !== videoSrc) {
-            document.getElementById(playingVideo).pause();
+
+    const handleScrollTo = (ref) => {
+        if (ref.current) {
+            ref.current.scrollIntoView({ behavior: 'smooth' });
         }
-        setPlayingVideo(videoSrc);
-    };
-
-    const handleVideoPause = () => {
-        setPlayingVideo(null);
-    };
-
-    const handleVideoClick = (videoSrc) => {
-        if (playingVideo === videoSrc) {
-            document.getElementById(videoSrc).pause();
-            setPlayingVideo(null);
-        } else {
-            setPlayingVideo(videoSrc);
-        }
-    };
-
-    const preventSeeking = (e) => {
-        if (e.target.seeking) {
-            e.target.currentTime = e.target.lastCurrentTime || 0;
-        }
-    };
-
-    const updateLastCurrentTime = (e) => {
-        e.target.lastCurrentTime = e.target.currentTime;
     };
 
     return (
         <div className="all-2">
             <div className="header-batafsil">
-                <img className="header-img" src={a} alt=""/>
+                <img className="header-img" src={a} alt="" />
                 <div className={"header-group"}>
-                    <p className="group-tours">Group Tours</p>
-                    <p className="about">About Us</p>
-                    <p className="contact">Contact Us</p>
+                    <p className="group-tours" onClick={() => handleScrollTo(groupToursRef)}>Group Tours</p>
+                    <p className="about" onClick={() => handleScrollTo(aboutUsRef)}>About Us</p>
+                    <p className="contact" onClick={() => handleScrollTo(contactRef)}>Contact Us</p>
                 </div>
                 <div className={"header-imgs"}>
-                    <img className="what" src={q} alt=""/>
-                    <img className="teleg" src={w} alt=""/>
-                    <img className="face" src={e} alt=""/>
-                    <img className="ins" src={r} alt=""/>
+                    <img className="what" src={q} alt="" />
+                    <img className="teleg" src={w} alt="" />
+                    <img className="face" src={e} alt="" />
+                    <img className="ins" src={r} alt="" />
                 </div>
-                <img className="black" src={t} alt=""/>
+                <img className="black" src={t} alt="" />
                 <p className="eng">ENG</p>
-                <img className="tel" src={y} alt=""/>
+                <img className="tel" src={y} alt="" />
                 <p className="number">+998901234567</p>
             </div>
             <p className="tour">{tourTitle}ga sayohat</p>
@@ -116,80 +95,76 @@ function Batafsil() {
             <p className="price">Price: {totalCost}</p>
             <button onClick={toOrder} className="order-batafsil">Order</button>
 
-            <div className="groups">
-
+            <div className="groups" ref={groupToursRef}>
                 {videoSrc && (
                     <video
                         id={videoSrc}
                         src={videoSrc}
                         controls
                         className="video"
-                        ref={videoRef}
-                        onPlay={() => handleVideoPlay(videoSrc)}
-                        onPause={handleVideoPause}
-                        onClick={() => handleVideoClick(videoSrc)}
-                        onSeeking={preventSeeking}
-                        onTimeUpdate={updateLastCurrentTime}
                     />
                 )}
-                {imageSrc && <img src={imageSrc} className="image-batafsil" alt="Tour Image"/>}
+                {imageSrc && <img src={imageSrc} className="image-batafsil" alt="Tour Image" />}
             </div>
-
             {batafsil.map((item, index) => (
                 <div
                     key={index}
                     className="batafsil-item"
-                    style={{top: `${initialTop + index * increment}px`}}
+                    style={{ top: `${initialTop + index * increment}px` }}
                 >
                     <img src={`http://localhost:8082/files/img?name=${item.photo}`} className="image-batafsil2"
-                         alt={`Day ${item.day} Image`}/>
+                         alt={`Day ${item.day} Image`} />
                     <div className="batafsil-details">
                         <p className="batafsil-title">Day: {item.day}</p>
                         <h3 className="batafsil-subtitle">{item.title}</h3>
                         <p className="batafsil-description">{item.description}</p>
-                        <img className={"rasmcha"} src={p} alt=""/>
+                        <img className={"rasmcha"} src={p} alt="" />
                         <p className="batafsil-description2">{item.tour.description2}</p>
                     </div>
                 </div>
             ))}
-            <p className="add">Additional Information</p>
-            <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
-                commodo sagittis, sapien dui mattis dui, non pulvinar
-                lorem felis nec erat Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar
-                lorem felis nec eratLorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar
-                lorem felis nec eratLorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nunc maximu </p>
+            <div ref={aboutUsRef}>
+                <p className="add">Additional Information</p>
+                <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                    commodo sagittis, sapien dui mattis dui, non pulvinar
+                    lorem felis nec erat Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar
+                    lorem felis nec eratLorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar
+                    lorem felis nec eratLorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc maximu </p>
+            </div>
             <p className="comment-batafsil">Comment</p>
-            <Comment/>
-            <div className="footer-container">
-                <div className="you">You can dream, create, design, and buildsa the most <br/> wonderful place.</div>
-                <img className="footer-img" src={u} alt="Phone Icon"/>
+            <Comment />
+            <div className="footer-container" ref={contactRef}>
+                <div className="you">You can dream, create, design, and buildsa the most <br /> wonderful place.</div>
+                <img className="footer-img" src={u} alt="Phone Icon" />
                 <p className="num2">+0123 456 987, +0123 456 987</p>
-                <img className="footer-img2" src={i} alt="Email Icon"/>
+                <img className="footer-img2" src={i} alt="Email Icon" />
                 <p className="email">info@example.com</p>
-                <img className="footer-img3" src={o} alt="Web Icon"/>
+                <img className="footer-img3" src={o} alt="Web Icon" />
                 <p className="www">www.example.com</p>
             </div>
-            <div className="footer-group">- Group Us</div>
-            <div className="footer-group2">- About Us</div>
-            <div className="footer-group3">- Connection</div>
-            <div className="footer-group4">- Comment</div>
-            <div className="support">Support:</div>
-            <img src={w} className="teleg-3" alt=""/>
-            <img src={q} className="what-3" alt=""/>
-            <p className="teleg-comment">t.me/barlasvoyageAdmin</p>
-            <p className="num3">+9988888888</p>
-            <div className="img-icon">
-                <img className="teleg-4" src={q} alt="Telegram"/>
-                <img className="what-4" src={w} alt="WhatsApp"/>
-                <img src={e} alt="Facebook"/>
-                <img src={r} alt="Instagram"/>
+            <div ref={footerRef}>
+                <div className="footer-group">- Group Us</div>
+                <div className="footer-group2">- About Us</div>
+                <div className="footer-group3">- Connection</div>
+                <div className="footer-group4">- Comment</div>
+                <div className="support">Support:</div>
+                <img src={w} className="teleg-3" alt="" />
+                <img src={q} className="what-3" alt="" />
+                <p className="teleg-comment">t.me/barlasvoyageAdmin</p>
+                <p className="num3">+9988888888</p>
+                <div className="img-icon">
+                    <img className="teleg-4" src={q} alt="Telegram" />
+                    <img className="what-4" src={w} alt="WhatsApp" />
+                    <img src={e} alt="Facebook" />
+                    <img src={r} alt="Instagram" />
+                </div>
             </div>
             <Rodal visible={modalVisible} onClose={handleCloseModal} height={400} width={700}>
                 <div className="rodal-content">
-                    <EnquiryForm/>
+                    <EnquiryForm />
                 </div>
             </Rodal>
         </div>
