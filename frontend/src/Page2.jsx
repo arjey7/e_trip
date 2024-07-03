4ea645b7b9013a779842725640471d1300812efa
Ismoil <mahbubatilaeva@gmail.com>
2024-07-03 17:45:17 +0500

    page 2

--- frontend/src/Page2.jsx
+++ frontend/src/Page2.jsx
@@ -275,9 +275,10 @@ function getContext(){
 
             <div className={"chiziq5"}></div>
             <p className={"dest"}>О туре (Дополнительно можно приобрести:) </p>
+            <hr width={900}/>
             {context.map((item,index)=>(
-                <div>
-                    <p style={{display: "flex"}} className={"textcha"}>- {item.text} <p className={"byn"}> {item.priceByn} BYN</p><p
+                <div style={{marginTop:'-25px'}}>
+                    <p style={{display: "flex"}} className={"textcha"}>- {item.text} <p className={"byna"}> {item.priceByn} BYN</p><p
                         className={"eur"}>(~{item.priceEur}
                         EUR)</p></p>
                 </div>
